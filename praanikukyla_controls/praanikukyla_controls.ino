cb94482cbc66efc6a6d90a16d0e8a1b9956aedc0
Taavi Tänavsuu <tanavst@gmail.com>
2022-01-02 11:47:15 +0200

    Added block 5, added commands for turnout 1, added methods for turnout servos, cleaned unnecessary code, added comments.

--- praanikukyla_controls/praanikukyla_controls.ino
+++ praanikukyla_controls/praanikukyla_controls.ino
@@ -1,30 +1,44 @@
 #include <SoftwareSerial.h>
+#include <Servo.h>
 
 #define BLUETOOTH_MODULE_BAUD_RATE 9600
 
-// MOTOR-DRIVER L298
+// Pins for controlling two trains (actually just sets of blocks of railroad)
+// independently of each other using a L298 DC motor controller.
 #define TRAIN1_MOTOR_ENA 9
 #define TRAIN1_MOTOR_IN1 8
 #define TRAIN1_MOTOR_IN2 7
 
+// Pins for switching on/off blocks of railroad.
+// Block 1 is always on, there is no switch for it.
 #define BLOCK2_ENA 12
 #define BLOCK3_ENA 4
 #define BLOCK4_ENA 22
+#define BLOCK5_ENA 24
 
+// Pins for controlling turnouts
+#define TURNOUT_1_SERVO 30
+#define TURNOUT_1_SWITCH 31
+
+// Turnout servo on times
+#define MOVEMENT_TIME_FOR_TURNOUT_CHANGE 600
+
+// Speeds of the turnout (continuous rotation) servos.
+#define SERVO_SPEED_STOP 90
+// There is some inprecision of these servos: the left and right correct speeds are not at symmetric distances from the stop speed.
+#define SERVO_SPEED_LEFT 80
+#define SERVO_SPEED_RIGHT 102
+
+// Constants and global variables related to reading commands from Bluetooth module
 #define MAX_COMMAND_LENGTH 100
-
 const char END_OF_COMMAND_CHAR = '\n';
-
-SoftwareSerial Bluetooth(11, 10); // RX, TX
-
 char cmd[MAX_COMMAND_LENGTH];
 unsigned int cmdIndex;
 
-// 24 speed
-//byte speedArray [] = {20,30,40,50,60,70,80,90,100,110,120,130,140,150,160,170,180,190,200,210,220,230,240,255};
+SoftwareSerial Bluetooth(11, 10); // RX, TX
+
 bool directionForwardTrain1 = true;
-unsigned int requestedTrainSpeed_1 = 0;
-unsigned int selectedTrainSpeed_1 = 0;
+unsigned int trainSpeed_1 = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -48,6 +62,10 @@ void setup() {
   pinMode(BLOCK2_ENA, OUTPUT);
   pinMode(BLOCK3_ENA, OUTPUT);
   pinMode(BLOCK4_ENA, OUTPUT);
+  pinMode(BLOCK5_ENA, OUTPUT);
+
+  // Initializing turnout switches
+  pinMode(TURNOUT_1_SWITCH, INPUT);
 
   // Set default direction of train 1 to FORWARD
   setDirectionTrain1(true);
@@ -63,6 +81,10 @@ void setup() {
   // Block 4 is enabled by default:
   Serial.println("DEBUG - setup - enabling block 4");
   digitalWrite(BLOCK4_ENA, HIGH);
+
+  // Block 5 is disabled by default:
+  Serial.println("DEBUG - setup - disabling block 5");
+  digitalWrite(BLOCK5_ENA, LOW);
 
 }
 
@@ -93,7 +115,7 @@ boolean cmdStartsWith(const char *st) {
 void executeCommand() {
   
   if( cmdStartsWith("speed_train1 ") ) {
-    requestedTrainSpeed_1 = atoi(cmd + 13);
+    trainSpeed_1 = atoi(cmd + 13);
     setSpeedTrain1();
   }
   
@@ -127,76 +149,45 @@ void executeCommand() {
     digitalWrite(BLOCK4_ENA, LOW);
   }
   
+  if( cmdStartsWith("block_5_on") ) {
+    Serial.println("DEBUG - executeCommand - enabling block 5");
+    digitalWrite(BLOCK5_ENA, HIGH);
+  }
+    
+  if( cmdStartsWith("block_5_off") ) {
+    Serial.println("DEBUG - executeCommand - disabling block 5");
+    digitalWrite(BLOCK5_ENA, LOW);
+  }
+
+  if( cmdStartsWith("turnout_1_calibrate") ) {
+    Serial.println("DEBUG - executeCommand - calibrating turnout 1");
+    calibrateTurnoutServo(TURNOUT_1_SERVO, TURNOUT_1_SWITCH, SERVO_SPEED_RIGHT, SERVO_SPEED_LEFT, SERVO_SPEED_STOP);
+  }
+ 
+  if( cmdStartsWith("turnout_1_straight") ) {
+    Serial.println("DEBUG - executeCommand - turning turnout 1 straight");
+    // Turnout 1 is a left turnout. So when the turnout mechanism has moved to left, train goes straight. 
+    // when turnout mechanism has moved to right, train turns left.
+    turnTurnoutServo(TURNOUT_1_SERVO, TURNOUT_1_SWITCH, HIGH, SERVO_SPEED_LEFT, MOVEMENT_TIME_FOR_TURNOUT_CHANGE, SERVO_SPEED_STOP);
+  }
+ 
+  if( cmdStartsWith("turnout_1_turn") ) {
+    Serial.println("DEBUG - executeCommand - turning turnout 1 turn");
+    // Turnout 1 is a left turnout. So when the turnout mechanism has moved to right, train turns left.
+    turnTurnoutServo(TURNOUT_1_SERVO, TURNOUT_1_SWITCH, LOW, SERVO_SPEED_RIGHT, MOVEMENT_TIME_FOR_TURNOUT_CHANGE, SERVO_SPEED_STOP);
+  }
+ 
   if( cmdStartsWith("direction_train1") ) {
     setDirectionTrain1(!directionForwardTrain1);
   }
 }
 
-unsigned int getSelectedSpeedFromRequestedSpeed(unsigned int *requestedSpeed) {
-  return *requestedSpeed;
-/*  if (*requestedSpeed > 10 && *requestedSpeed <= 20) {
-    return 20;
-  } else if (*requestedSpeed > 20 && *requestedSpeed <= 30) {
-    return 30;
-  } else if (*requestedSpeed > 30 && *requestedSpeed <= 40) {
-    return 40;
-  } else if (*requestedSpeed > 40 && *requestedSpeed <= 50) {
-    return 50;
-  } else if (*requestedSpeed > 50 && *requestedSpeed <= 60) {
-    return 60;
-  } else if (*requestedSpeed > 60 && *requestedSpeed <= 70) {
-    return 70;
-  } else if (*requestedSpeed > 70 && *requestedSpeed <= 80) {
-    return 80;
-  } else if (*requestedSpeed > 80 && *requestedSpeed <= 90) {
-    return 90;
-  } else if (*requestedSpeed > 90 && *requestedSpeed <= 100) {
-    return 100;
-  } else if (*requestedSpeed > 100 && *requestedSpeed <= 110) {
-    return 110;
-  } else if (*requestedSpeed > 110 && *requestedSpeed <= 120) {
-    return 120;
-  } else if (*requestedSpeed > 120 && *requestedSpeed <= 130) {
-    return 130;
-  } else if (*requestedSpeed > 130 && *requestedSpeed <= 140) {
-    return 140;
-  } else if (*requestedSpeed > 140 && *requestedSpeed <= 150) {
-    return 150;
-  } else if (*requestedSpeed > 150 && *requestedSpeed <= 160) {
-    return 160;
-  } else if (*requestedSpeed > 160 && *requestedSpeed <= 170) {
-    return 170;
-  } else if (*requestedSpeed > 170 && *requestedSpeed <= 180) {
-    return 180;
-  } else if (*requestedSpeed > 180 && *requestedSpeed <= 190) {
-    return 190;
-  } else if (*requestedSpeed > 190 && *requestedSpeed <= 200) {
-    return 200;
-  } else if (*requestedSpeed > 200 && *requestedSpeed <= 210) {
-    return 210;
-  } else if (*requestedSpeed > 210 && *requestedSpeed <= 220) {
-    return 220;
-  } else if (*requestedSpeed > 220 && *requestedSpeed <= 230) {
-    return 230;
-  } else if (*requestedSpeed > 230 && *requestedSpeed <= 240) {
-    return 240;
-  } else if (*requestedSpeed > 240 && *requestedSpeed <= 255) {
-    return 255;
-  } else {
-    return 0; 
-  }*/
-}
-
 void setSpeedTrain1() {
-    if (requestedTrainSpeed_1 >= 0 && requestedTrainSpeed_1 <= 255) {
-      Serial.print("DEBUG - setSpeedTrain1 - requestedTrainSpeed_1 = ");
-      Serial.print(requestedTrainSpeed_1);
+    if (trainSpeed_1 >= 0 && trainSpeed_1 <= 255) {
+      Serial.print("DEBUG - setSpeedTrain1 - trainSpeed_1 = ");
+      Serial.print(trainSpeed_1);
       Serial.println();
-      selectedTrainSpeed_1 = getSelectedSpeedFromRequestedSpeed(&requestedTrainSpeed_1);
-      Serial.print("DEBUG - setSpeedTrain1 - selectedTrainSpeed_1 = ");
-      Serial.print(selectedTrainSpeed_1);
-      Serial.println();
-      analogWrite(TRAIN1_MOTOR_ENA, selectedTrainSpeed_1);
+      analogWrite(TRAIN1_MOTOR_ENA, trainSpeed_1);
     }
 }
 
@@ -211,4 +202,85 @@ void setDirectionTrain1(const bool directionForward) {
     digitalWrite(TRAIN1_MOTOR_IN1, LOW);
     digitalWrite(TRAIN1_MOTOR_IN2, HIGH);
   }
+}
+
+/* 
+ * I have Weinert Modellbau 74310 turnout point mechanisms (because i wanted the rotating turnout lanterns - 72381 and the lke - in my layout), but some old SG90 continuous 
+ * rotation microservos, which is probably not the best combination.  Standard servos would be better, i guess.  So i'm relying on precise millisecond timing to turn my 
+ * turnouts with these servos. I'm using the "frog" polarity switch of the turnout mechanism to detect which state the turnout is in. Initially i need to calibrate the 
+ * turnout mechanims and servo, to make sure the turnout movement happens around the center of the possible movement range of the turnout mechanism, and the switching 
+ * happens somewhere in the middle of this movement. 
+ */
+void calibrateTurnoutServo(const int servoControlPin, const int turnoutSwitchInputPin, const int servoSpeedRight, const int servoSpeedLeft, const int servoSpeedStop) {
+  const unsigned int MOVEMENT_COUNT_FOR_CALIBRATION_LIMIT = 100;
+  const unsigned int MOVEMENT_TIME_FROM_SWITCH_POSITION_TO_LEFT_END = 30;
+  int turnoutSwitchState = digitalRead(turnoutSwitchInputPin);
+  debugSwitchState(turnoutSwitchInputPin, String("beginning of calibration"));
+  Servo myservo;
+  myservo.attach(servoControlPin);  // attaches the servo on pin 9 to the servo object
+  myservo.write(servoSpeedStop);
+  delay(100);
+  unsigned int countForSafety = 0;
+  // If the switch is on (we are somewhere in the left side), then first move to right until
+  // the switch switches off.
+  turnoutSwitchState = digitalRead(turnoutSwitchInputPin);
+  while (turnoutSwitchState == HIGH && countForSafety <= MOVEMENT_COUNT_FOR_CALIBRATION_LIMIT) {
+    myservo.write(servoSpeedRight);
+    delay(10);
+    turnoutSwitchState = digitalRead(turnoutSwitchInputPin);
+    countForSafety++;
+  }
+  // Move left in small steps until the switch goes on.
+  countForSafety = 0;
+  while (turnoutSwitchState == LOW && countForSafety <= MOVEMENT_COUNT_FOR_CALIBRATION_LIMIT) {
+    myservo.write(servoSpeedLeft);
+    delay(10);
+    turnoutSwitchState = digitalRead(turnoutSwitchInputPin);
+    countForSafety++;
+  }
+  // Now we have found the point where the switch happens, so we know where we are.  
+  // Moving left for the experimentally found anmount of time (depends on the screw position of the switch), 
+  // to reach the left end of the switchable area:
+  myservo.write(servoSpeedLeft);
+  delay(MOVEMENT_TIME_FROM_SWITCH_POSITION_TO_LEFT_END);
+  myservo.write(servoSpeedStop);
+  // wait a bit:
+  delay(100);
+  debugSwitchState(turnoutSwitchInputPin, String("end of calibration"));
+  myservo.detach();
+}
+
+void turnTurnoutServo(const int servoControlPin, const int turnoutSwitchInputPin, const int turnoutSwitchStateWhenDone, const int servoSpeed, const int turnoutMovementTimeMillis, const int servoSpeedStop) {
+  int turnoutSwitchState = digitalRead(turnoutSwitchInputPin);
+  if (turnoutSwitchState != turnoutSwitchStateWhenDone) {
+    Servo myservo;
+    myservo.attach(servoControlPin);
+    debugSwitchState(turnoutSwitchInputPin, String("beginning of turn"));
+    myservo.write(servoSpeed);
+    delay(turnoutMovementTimeMillis);
+    myservo.write(servoSpeedStop);
+    delay(100);
+    debugSwitchState(turnoutSwitchInputPin, String("end of turn"));
+    myservo.detach();
+  }
+  turnoutSwitchState = digitalRead(turnoutSwitchInputPin);
+  if (turnoutSwitchState != turnoutSwitchStateWhenDone) {
+    Serial.print("#### ERROR! #### - turnout switch is not in expected state after turn!");
+  }
+}
+
+
+void debugSwitchState(const int turnoutSwitchInputPin, String switchStateMoment) {
+  int turnoutSwitchState = digitalRead(turnoutSwitchInputPin);
+  Serial.print("Switch state (");
+  Serial.print(switchStateMoment);
+  Serial.print(") : ");
+  if (turnoutSwitchState == HIGH) {
+    Serial.print("<HIGH>");
+  } else if (turnoutSwitchState == LOW) {
+    Serial.print("<LOW>");
+  } else {
+    Serial.print("<UNDEFINED>");
+  }
+  Serial.print("\n");  
 }
